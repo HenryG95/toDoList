8cc9f4c6cbd0279363085ad23f7400794fb96d86
Henry <henrygabrielgm@gmail.com>
2021-09-02 10:43:38 -0400

    css update and buttons

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import { inbox } from "./loader.js";
+
+inbox.addEventListener("click",() => {
+    console.log("hi");
+});
+
+console.log('working');
---
+++ src/loader.js
@@ -0,0 +1,4 @@
+export var inbox = document.getElementById("inbox-button");
+export var newProject = document.getElementById("project-button");
+export var newTask = document.getElementById("task-button");
+
